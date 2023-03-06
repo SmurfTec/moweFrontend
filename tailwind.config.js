20e5c450b877e78cc42794bcf7604d08432f9db5
Hamzah <hamzahejaz88@gmail.com>
2023-03-06 21:58:47 +0500

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        blue:{
-          glacier:"#E6F2ED"
+      colors: {
+        blue: {
+          glacier: "#E6F2ED",
+        },
+        gray:{
+          light:"#CACACA",
+          charcol:"#1B1B1B"
         }
-      }
+      },
+      fontSize: {
+        c2xl: ["1.5rem", "2.25rem"],
+      },
     },
   },
   plugins: [],
-}
+};
