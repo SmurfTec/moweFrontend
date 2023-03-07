9d1c66b817fa1e912237588b1a9244ee439b7bf8
Hamzah <hamzahejaz88@gmail.com>
2023-03-07 23:39:48 +0500

    landing-screen

--- src/Layout/LandingLayout.js
+++ src/Layout/LandingLayout.js
@@ -1,9 +1,12 @@
+import { CarouselImages } from "components/LandingSection/CarouselImages";
 import { Footer } from "components/LandingSection/Footer";
+import { Navbar } from "components/LandingSection/Navbar";
 import { Review } from "components/LandingSection/Review";
 
 export const LandingLayout = () => {
   return (
     <>
+      <Navbar />
       <Review />;
       <Footer />
     </>
--- src/components/LandingSection/Review.js
+++ src/components/LandingSection/Review.js
@@ -6,13 +6,16 @@ import LS1 from "assets/LS1.png";
 import L2 from "assets/LS2.png";
 export const Review = () => {
   return (
-    <div className="bg-blue-glacier h-[40rem] mt-[10%] flex justify-around items-center">
-      <LandingSectionReview1 className="h-11/12 object-cover" />
-      <LandingSectionReview2 className="h-11/12 object-cover" />
+    <div className="bg-blue-glacier md:h-[40rem] mt-[10%] w-full flex justify-around items-center overflow-auto">
+      {/* <LandingSectionReview1 className="md:h-11/12 object-cover" />
+      <LandingSectionReview2 className="md:h-11/12 object-cover" /> */}
       {/* <LandingSectionReview3 className="h-11/12 object-cover" /> */}
       {/* <LandingSectionReview4 className="h-11/12 object-cover" /> */}
-      <img src={LS1} className="h-11/12 object-cover"/>
-      <img src={L2} className="h-11/12 object-cover"/>
+      <img src={LS1} className="md:h-11/12 object-cover"/>
+      <img src={L2} className="md:h-11/12 object-cover"/>
+      <img src={L2} className="md:h-11/12 object-cover"/>
+      <img src={L2} className="md:h-11/12 object-cover"/>
+
 
     </div>
   )
