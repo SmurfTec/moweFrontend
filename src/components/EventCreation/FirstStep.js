af4e3706453e05cf1a7e99fb3b77608aedeb6cf7
Hamzah <hamzahejaz88@gmail.com>
2023-03-20 21:32:12 +0500

    forms-translation

--- src/components/EventCreation/FirstStep.js
+++ src/components/EventCreation/FirstStep.js
@@ -1,9 +1,4 @@
-import { ReactComponent as AppLogo } from "assets/AppLogo.svg";
 import ModalBasic, { Modal } from "components/Modal/BasicModal";
-import LoginImage from "assets/Login.jpg";
-import FacebookImage from "assets/fb.png";
-import Google from "assets/google.png";
-import OtpInput from "react-otp-input";
 
 import { ReactComponent as CrossIcon } from "assets/Cross.svg";
 import { ReactComponent as BackIcon } from "assets/BackArrow.svg";
@@ -21,30 +16,55 @@ import { CheckBox } from "components/Common/CheckBox/CheckBox";
 export const EMAIL_INVALID = "Email is Invalid";
 
 export const FirstStep = ({ modalOpen = false, setModalOpen }) => {
-  const [resetPassword, setResetPassword] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
   return (
     <ModalBasic open={modalOpen} onClose={() => setModalOpen(false)}>
-      <div className="bg-white  w-[74rem] rounded-lg  flex flex-col justify-between pt-12 pb-12 pr-8 pl-8 ">
-        <div className="flex flex-col gap-10 pb-8">
-          <Header setModalOpen={setModalOpen} />
-          <Content7 />
+      <div className="bg-white  w-[74rem] min-h-[44rem] rounded-lg  flex flex-col justify-between pt-12 pb-12 pr-8 pl-8 ">
+        <div className="flex flex-col gap-10 pb-8 min-h-[40rem]">
+          <Header setModalOpen={setModalOpen} currentStep={currentStep} />
+          <EventCreationForms currentStep={currentStep} />
         </div>
-        <Footer />
+        <Footer currentStep={currentStep} setCurrentStep={setCurrentStep} />
       </div>
     </ModalBasic>
   );
 };
+const EventCreationForms = ({ currentStep }) => {
+  switch (currentStep) {
+    case 1:
+      return <Content />;
+    case 2:
+      return <Content />;
+    case 3:
+      return <Content3 />;
+    case 4:
+      return <Content4 />;
+    case 5:
+      return <Content5 />;
+    case 6:
+      return <Content6 />;
+    case 7:
+      return <Content7 />;
 
-const Header = ({ isFirstStep = false, setModalOpen }) => {
+    default:
+      return "No case found";
+  }
+};
+const Header = ({ isFirstStep = false, setModalOpen, currentStep }) => {
   return (
     <div className="flex justify-between w-full items-center">
       <div className="flex items-center gap-2 text-lg cursor-pointer">
-        <BackIcon
-          className="h-5 w-5 cursor-pointer"
-          // onClick={() => setModalOpen(false)}
-        />
-        <div className="text-xl text-gray-dark">
-          {isFirstStep ? "Back" : "Step 2 of 7"}
+        {currentStep === 1 && (
+          <BackIcon
+            className="h-5 w-5 cursor-pointer"
+            // onClick={() => setModalOpen(false)}
+          />
+        )}
+        <div className="text-cmd2 text-gray-dark flex items-center gap-2">
+          <div>{isFirstStep ? "Back" : `Paso ${currentStep} de 7`}</div>
+          <div className="text-csm text-gray-dark">
+            (Se podrán modificar los datos después)
+          </div>
         </div>
       </div>
       <div>
@@ -56,21 +76,32 @@ const Header = ({ isFirstStep = false, setModalOpen }) => {
     </div>
   );
 };
-const Footer = ({ isFirstStep = false }) => {
+const Footer = ({ currentStep, setCurrentStep }) => {
+  const isFirstStep = currentStep === 1;
+  const isLastStep = currentStep === 7;
+
   return (
     <div className="flex gap-3 justify-end">
       <Button
-        btnText={isFirstStep ? "Step 1 of 7" : "Previous step"}
+        btnText={isFirstStep ? "Paso 1 de 7" : "Anterior"}
         className={ClassNames(
-          "w-40",
+          "w-40 text-gray-dark",
           isFirstStep ? "bg-white" : "w-56 bg-gray-thin",
         )}
-        onClick={() => {}}
+        onClick={() => {
+          if (currentStep > 1 && currentStep <= 7) {
+            setCurrentStep((prevState) => prevState - 1);
+          } else setCurrentStep(1);
+        }}
       />
       <Button
-        btnText="Log In"
-        className="w-40 !bg-green-teal shadow-lg"
-        onClick={() => {}}
+        btnText={isLastStep ? "Ya lo tienes" : "Siguiente"}
+        className="w-40 !bg-green-teal shadow-lg text-white"
+        onClick={() => {
+          if (currentStep < 7) {
+            setCurrentStep((prevState) => prevState + 1);
+          } else setCurrentStep(7);
+        }}
       />
     </div>
   );
@@ -79,16 +110,15 @@ const Content = () => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
-    <div>
+    <div className="min-h-[30rem] flex flex-col gap-10">
       <div className="text-c2xl font-medium">
-        Time to customize your invitation! tell us about your wedding day!{" "}
+        Cuéntanos más detalles de tu boda!
       </div>
       <div className="text-c2lg">
-        Tell us your names! this will be shown at the very beginning of the
-        invitation
+        ¿Cómo os llamáis? (Así aparecerá en vuestra invitación)
       </div>
       <TextArea placeHolder="Jaume & Yolanda" isOptional={false} rows={3} />
-      <div className="text-c2lg">Time and Date of the wedding</div>
+      <div className="text-c2lg">Fecha y hora del evento</div>
       <DatePicker
         selected={startDate}
         onChange={(date) => setStartDate(date)}
@@ -101,16 +131,14 @@ const Content = () => {
   );
 };
 const Content3 = () => {
-  const [startDate, setStartDate] = useState(new Date());
   const [isChecked, setIsChecked] = useState(false);
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-10 min-h-[35rem]">
       <div className="text-c2xl font-medium">
-        how will the guests arrive to your wedding, is there any special
-        indication?
+      ¿Cómo quieres que lleguen los invitados a tu boda? Hay alguna indicación?
       </div>
       <div className="text-c2lg flex items-center gap-3">
-        do you offer transport to your guests?
+      ¿Habrá transporte? Cuéntaselo a los invitados
         <div className="flex h-6 items-center">
           <input
             id="comments"
@@ -129,7 +157,7 @@ const Content3 = () => {
       <div className="flex justify-between items-center gap-20">
         <div className="flex flex-col gap-4 w-1/2">
           <div className="text-c2lg">
-            Is there any special parking indication?
+          ¿Hay alguna indicación especial para aparcar?
           </div>
           <div className="w-full">
             <TextArea
@@ -141,8 +169,7 @@ const Content3 = () => {
         </div>
         <div className="flex flex-col w-1/2 gap-4 -mt-[3%]">
           <div className="text-c2lg ">
-            is thera a QR or jpg image needed to access the event place or
-            parking?
+          ¿Tienes un QR de acceso al parking? Compártelo aquí con tus invitados!
           </div>
           <div className="">
             <UploadFile
@@ -157,20 +184,18 @@ const Content3 = () => {
           </div>
         </div>
       </div>
-      We’re offering a direct link to hotels nearby: choose location
+      Vamos a ofrecer a los asistentes, hoteles cercanos al lugar que elijas
     </div>
   );
 };
 const Content4 = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [isChecked, setIsChecked] = useState(false);
+  
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-10 min-h-[35rem]">
       <div className="flex items-center">
         <div className="flex flex-col gap-4 w-full">
           <div className="text-c2lg">
-            the Dresscode is a must! Tell your guests how should they be dressed
-            for the big day!
+          ¡El Dresscode es importante! Cuéntales el estilo de tu boda a los invitados!
           </div>
           <div className="w-10/12">
             <TextArea
@@ -182,16 +207,16 @@ const Content4 = () => {
         </div>
       </div>
       <div className="flex items-center gap-5">
-        ¿need help with this? get inspired here:
+      ¿No sabes que frase poner? Inspírate aquí:
         <Button
-          btnText={"Dresscode examples"}
-          className={ClassNames("w-60 bg-gray-thin py-1")}
+          btnText={"Ejemplos dresscode"}
+          className={ClassNames("w-60 bg-gray-thin !py-1")}
           onClick={() => {}}
         />
       </div>
       <div className="w-2/3 cmd">
-        Mowe Interactvie includes a real time weather forecast for the date &
-        location of your wedding: please select the location we should consider
+      Vamos a informar de la meteorología esperada a tus invitados! 
+¿En qué ubicación queréis verla?
       </div>
     </div>
   );
@@ -204,12 +229,11 @@ const Content5 = () => {
 
   return (
     <div className="flex flex-col gap-6">
-      <div className="font-medium text-c2xl">Let’s get into Social media! </div>
+      <div className="font-medium text-c2xl">¡Vamos a petar las redes sociales! </div>
       <div className="flex items-center">
         <div className="flex flex-col gap-4 w-full">
           <div className="text-cmd1 text-gray-gunMetal">
-            Add your favorite hashtag, we’ll ask your guests to use it so you
-            won’t miss any uploaded picture!
+          Añade tu hashtag favorito para que todo el mundo lo use en sus fotos y vídeos!
           </div>
           <div className="w-10/12">
             <TextArea
@@ -222,8 +246,7 @@ const Content5 = () => {
         </div>
       </div>
       <div className="flex">
-        Add your social media profile links so your guests can see your uploads
-        too!
+      No te pierdas ninguna foto de tus invitados! dales tus redes sociales y que te etiqueten!
       </div>
       <div className="flex gap-4 items-center">
         <CheckBox
@@ -232,10 +255,10 @@ const Content5 = () => {
         />
         <div className="text-c2lg w-40">Google Photos:</div>
         <InputField
-          className="w-[40rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Escribe aquí..."
           type="text"
           isRequired={true}
           onChange={(value) => {}}
@@ -248,10 +271,10 @@ const Content5 = () => {
         />
         <div className="text-c2lg w-40">Instagram:</div>
         <InputField
-          className="w-[40rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Escribe aquí..."
           type="text"
           isRequired={true}
           onChange={(value) => {}}
@@ -264,10 +287,10 @@ const Content5 = () => {
         />
         <div className="text-c2lg w-40">Tik Tok:</div>
         <InputField
-          className="w-[40rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Escribe aquí..."
           type="text"
           isRequired={true}
           onChange={(value) => {}}
@@ -280,10 +303,10 @@ const Content5 = () => {
         />
         <div className="text-c2lg w-40">Facebook:</div>
         <InputField
-          className="w-[40rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Escribe aquí..."
           type="text"
           isRequired={true}
           onChange={(value) => {}}
@@ -301,14 +324,14 @@ const Content6 = () => {
   return (
     <div className="flex flex-col gap-3">
       <div className="font-medium text-c2xl">
-        Would you like to receive gifts from your guests?{" "}
+      ¿Quieres recibir un regalo de tus invitados?
       </div>
-      <div className="text-xl">Tell them! </div>
+      <div className="text-xl"> ¡Hazlo ahora! Aparecerá en la sección de regalo de la invitación</div>
       <div className="flex items-center">
         <div className="flex flex-col gap-4 w-full">
           <div className="w-10/12">
             <TextArea
-              placeHolder="having you in our big day is enough but if you consider having a detail for us, this is what we want! "
+              placeHolder="Si nos queréis hacer un regalo de boda estaremos encantados..."
               isOptional={false}
               rows={4}
               className="bg-gray-snow"
@@ -317,51 +340,51 @@ const Content6 = () => {
         </div>
       </div>
       <div className="flex items-center gap-5">
-        ¿need help? Get inspired here:
+      ¿No sabes que frase poner? Inspírate aquí:
         <Button
-          btnText={"wedding gift examples"}
+          btnText={"Ejemplos regalos de boda"}
           className={ClassNames(
-            "w-fit bg-gray-thin py-1 font-extralight text-md",
+            "w-fit bg-gray-thin !py-1 font-extralight text-md",
           )}
           onClick={() => {}}
         />
       </div>
       <div className="flex">
         <div className="text-black text-c2lg">
-          Add your preferences to receive gifts
+        Opciones de regalo para los invitados
         </div>
       </div>
       <div className="flex gap-4 items-center">
         <div className="text-cmd w-40">Bizum:</div>
         <InputField
-          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[47rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Cuenta Bancaria:"
           type="text"
           isRequired={true}
           onChange={(value) => {}}
         />
       </div>
       <div className="flex gap-4 items-center">
-        <div className="text-cmd w-40">Bank account:</div>
+        <div className="text-cmd w-40">Cuenta Bancaria:</div>
         <InputField
-          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[47rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Cuenta Bancaria:"
           type="text"
           isRequired={true}
           onChange={(value) => {}}
         />
       </div>
       <div className="flex gap-4 items-center">
-        <div className="text-cmd w-40">wedding list:</div>
+        <div className="text-cmd w-40">Lista de bodas:</div>
         <InputField
-          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[47rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Cuenta Bancaria:"
           type="text"
           isRequired={true}
           onChange={(value) => {}}
@@ -370,17 +393,17 @@ const Content6 = () => {
       <div className="flex gap-4 items-center">
         <div className="text-cmd w-40">Paypal-Me:</div>
         <InputField
-          className="w-[45rem] shadow-md bg-gray-snow border-gray-platinum"
+          className="w-[47rem] shadow-md bg-gray-snow border-gray-platinum"
           className1="bg-gray-snow py-[.3rem]"
           id="Contraseña"
-          placeholder="write here"
+          placeholder="Cuenta Bancaria:"
           type="text"
           isRequired={true}
           onChange={(value) => {}}
         />
       </div>
       <div className="flex gap-4 items-center mt-4">
-        <div className="text-c2lg">WE DON’T WANT GIFTS:</div>
+        <div className="text-c2lg">No quiero regalos:</div>
         <CheckBox
           isChecked={googleUrl}
           onChange={(prevState) => setGoogleUrl(!prevState)}
@@ -392,10 +415,10 @@ const Content6 = () => {
 const Content7 = () => {
   return (
     <div className="flex flex-col gap-3 h-96">
-      <div className="font-medium text-c2xl pl-8">We’re almost ready</div>
+      <div className="font-medium text-c2xl pl-8">Ya casi estamos!!</div>
       <div className="flex flex-col justify-center items-center h-full">
         <div className=" p-t-4 pb-4 w-9/12 text-c2lg">
-          Mowe interactive includes a customized url, choose yours now!
+        Tu invitación tendrá una URL personalizada, ¿Cuál quieres?
         </div>
         <div className="flex justify-between w-9/12">
           {" "}
@@ -409,10 +432,8 @@ const Content7 = () => {
             onChange={(value) => {}}
           />
           <Button
-            btnText={"Check availability"}
-            className={ClassNames(
-              "w-72 bg-gray-thin py-1  text-md",
-            )}
+            btnText={"Comprobar disponibilidad"}
+            className={ClassNames("w-72 bg-gray-thin py-1  text-md")}
             onClick={() => {}}
           />
         </div>
