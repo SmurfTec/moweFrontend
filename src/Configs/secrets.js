597ee9c0df9e71c0be4d4b24fcc86cdcc7079f9a
Saqib Javed Qureshi <saqib743@live.com>
2023-08-09 19:32:56 +0500

    added stripe integration

--- src/Configs/secrets.js
+++ src/Configs/secrets.js
@@ -1,1 +1,3 @@
-export const API_BASE = "http://localhost:8000/api/"
+export const API_BASE = "http://localhost:8000/api/";
+export const STRIPE_KEY =
+  "pk_test_51NdCZbJeMUkp1IBhL8HWKIa5dFvPu4LkPWcST6WuESFB5v8nlj0DDlPmFuRrAK2wcMRZKJmEyGwePIJrvJ89sClQ00lyCnQVXo";
